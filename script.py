2c29f0a8875c8841dcb0f3f3e5ae250b34d97d1a
Kerim Celik <fu5i1i3r@gmail.com>
2015-12-15 11:29:27 -0800

    small fixes made

--- script.py
+++ script.py
@@ -9,12 +9,14 @@ Save table
 
 Make header bold
 
-Make small figure, red line
+Make large figure, red line
 
 Plot data on chart
 
 Run analysis
 
 Discuss results
+
+# Analyze second file
 
 Read another file
